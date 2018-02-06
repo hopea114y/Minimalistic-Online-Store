04fdbca44e1e55d6f19be07ab78d580bc78c3674
hopea114y <serhioalfa@mail.ru>
2018-02-06 19:46:53 +0300

    Finish all desktop version

--- js/custom.js
+++ js/custom.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 // initMap() - функция инициализации карты
 function initMap() {
 	// Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
-	var centerLatLng = new google.maps.LatLng(52.217270, 24.026827);
+	var centerLatLng = new google.maps.LatLng(52.215514, 24.027539);
 	// Обязательные опции с которыми будет проинициализированна карта
 	var mapOptions = {
 		center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
